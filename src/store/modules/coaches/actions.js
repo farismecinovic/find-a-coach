41380a00f4f903d27e961be9f3d22549de731496
Faris <faris.mecinovic.np@gmail.com>
2021-09-20 19:29:25 +0200

    getting coaches from firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -22,4 +22,28 @@ export default {
 
     context.commit("registerCoach", { ...coachData, id: userId });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://find-a-coach-150b0-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = response.json();
+    console.log(responseData);
+    if (!response.ok) {
+      // error ...
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit("setCoaches", coaches);
+  },
 };
