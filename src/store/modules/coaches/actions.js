690a027e889ae1339321a7d96ae6557afc40ba84
Faris <faris.mecinovic.np@gmail.com>
2021-09-20 19:14:27 +0200

    integration with firebase and PUT request for coaches/id

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,5 +1,6 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
       id: context.rootGetters.userId,
       firstName: data.first,
@@ -8,6 +9,17 @@ export default {
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit("registerCoach", coachData);
+    const response = await fetch(
+      `https://find-a-coach-150b0-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      }
+    );
+    if (!response.ok) {
+      //error...
+    }
+
+    context.commit("registerCoach", { ...coachData, id: userId });
   },
 };
